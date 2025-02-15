24b67195799e68c00e0b525d20664ae0e9487b73
scandishoper <123@123>
2025-02-16 01:58:07 +0800

    Site updated: 2025-02-16 01:58:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/16/大一上/","2025/02/15/博客/","2025/02/13/hello-world/"];function toRandomPost(){
+var posts=["2025/02/15/博客/","2025/02/16/大一上/","2025/02/13/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
